518898dbb4c7dacb6e5a948f6465e731d348763a
ombhoir <79741241+ombhoir@users.noreply.github.com>
2021-08-21 13:02:37 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,74 @@
+function quizpage(){
+    location.href = "quiz.html"; 
+}
+let questions = [
+    {
+    id: 1,
+    question: "What's the name of Black Panther's kingdom?",
+    answer: "Wakanda",
+    options: [
+    "Makanda",
+    "Wakanda",
+    "Lambina",
+    "Waklanda"
+    ]
+    },
+    {
+    id: 2,
+    question: "Who is Thanos the son of?",
+    answer: "A'lars",
+    options: [
+    "Karen",
+    "A'lars",
+    "En Sabah Nur",
+    "Joe"
+    ]
+    },
+    {
+    id: 3,
+    question: "What is the name of Thor's mother?",
+    answer: "Frigga",
+    options: [
+    "Karen",
+    "Frigga",
+    "Aegir",
+    "Alfdis"
+    ]
+    },
+    {
+    id: 4,
+    question: "What was Captain America's height before he was transformed into a super-soldier?",
+    answer: "5'4",
+    options: [
+    "4'11",
+    "5'1",
+    "5'9",
+    "5'4"
+    ]
+    },
+    {
+    id: 5,
+    question: "What was the name of the dwarf on Nidavellir?",
+    answer: "Eitri",
+    options: [
+    "Steve",
+    "Walsh",
+    "Eitri",
+    "Erik"
+    ]
+    },
+    {
+    id: 6,
+    question: "Finish this line from Captain America: And Hulk, ___",
+    answer: "Smash",
+    options: [
+    "Smash",
+    "Kick ass",
+    "Do it",
+    "Good Luck"
+    ]
+    }
+    ];
+    
+
+    
